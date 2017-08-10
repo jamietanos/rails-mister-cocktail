1ecfa9dc8ee3ea29cf9ed62cacf77f3dc406f27f
Jamie Tan <jamie.enxuan@gmail.com>
2017-08-10 17:24:31 +0800

    Almost there

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :cocktails do
+ root "cocktails#index"
+ resources :cocktails do
     resources :doses, only: [ :index, :new, :create ]
   end
   resources :doses, only: [ :show, :edit, :update, :destroy ]
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Cocktail.destroy_all
+
+puts 'Creating cocktails...'
+cocktails_attributes = [
+  {
+    name:         "Jasmine's Pea Nest"
+  },
+  {
+    name:         "Sloppy Blowjobs"
+  },
+  {
+    name:         "Graveyard Digger"
+  }
+]
+Cocktail.create!(cocktails_attributes)
+puts "Finished!"
+
+puts 'Cleaning database...'
+Ingredients.destroy_all
+
+puts 'Creating cocktails...'
+ingredients_attributes = [
+  {
+    name:         "Boobie juice"
+  },
+  {
+    name:         "lime"
+  },
+  {
+    name:         "smelly socks"
+  }
+]
+Ingredient.create!(ingredients_attributes)
+puts "Finished!"
+
